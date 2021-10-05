34d2c4a8e5a042b16ebcadddfc125aef3d4d3dc2
BhavikBhundiya <69454958+BhavikBhundiya@users.noreply.github.com>
2021-10-05 19:06:14 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ print(lines)
 for i in range(0,len(lines)):
     data = lines[i].encode()
     name = base64.b64encode(data)
-    version, level, qr_name = myqr.run(
+    version,level,qr_name = myqr.run(
     str(name),
     level = 'H',
     version = 1,
@@ -20,5 +20,5 @@ for i in range(0,len(lines)):
     contrast = 1.0,
     brightness=1.0,
     save_name = str(lines[i]+'.bmp'),
-    save_name= os.getcwd(),
+    save_dir= os.getcwd(),
 )
